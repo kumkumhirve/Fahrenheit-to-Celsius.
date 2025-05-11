cc41a82124c1524bb331e3eec67e3b837be69ca5
kumkumhirve <kumkumhirve452@gmail.com>
2025-05-11 22:35:29 +0530

    Add files via upload

---
+++ far_cal.py
@@ -0,0 +1,9 @@
+# convert fahrenheit to celsius 
+
+'''Q6: Write a program to calculate temperature conversion from
+Fahrenheit to Celsius. (C=((F- 32)5/9)'''
+
+F = int(input("enter fahrenheit:"))
+
+C=(F-32)*5/9
+print(C)
